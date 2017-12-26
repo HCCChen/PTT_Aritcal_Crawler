8362e0973cdcff0b98c9b8bbe59bfb0b805a25a6
HCCChen <ccx97u@gmail.com>
2017-12-26 22:26:19 +0800

    [Develope] Get all article url and meta data from list
    Change list:
    - Get all url/meta data from page 1 to last
    - Rename some parameter
    
    To-Do
    - Get meta data from each article
    - Save these information to file

--- AritcalCrawler/crawler.py
+++ AritcalCrawler/crawler.py
@@ -7,6 +7,9 @@ PTT_URL = 'https://www.ptt.cc'
 
 # Get article link and relative information from article list
 def get_article_link(oriText):
+    if oriText.find('a') is None:
+        return
+
     href = oriText.find('a')['href']
     url = PTT_URL + href
 
@@ -37,15 +40,25 @@ def get_board_context(board, index):
     return resp
 
 
+# To-do: get meta data from article
+def get_article_meta_data(link):
+    print(link)
+    # Artical URL Example: https://www.ptt.cc/bbs/Baseball/M.1509090486.A.1EB.html
+    #resp = requests.get(
+    #    url = 'https://www.ptt.cc/bbs/Baseball/M.1509090486.A.1EB.html',
+    #    cookies={'over18': '1'}, verify=True, timeout=10
+    #)
+    #print(resp.text)
+    return
+
 if __name__ == '__main__':
     #===================================
     board = 'ToS'
     index = '0'
-    PTT_URL = 'https://www.ptt.cc'
     #===================================
+    # Firstly, get first page of article list.
     resp = get_board_context(board, index)
-    print(resp.text)
-
+#    print(resp.text)
 
     # init BeautifulSoup
     soup = BeautifulSoup(resp.text, 'html.parser')
@@ -55,22 +68,27 @@ if __name__ == '__main__':
         print('Server is too busy, skip it!')
         exit()
    
-    # Get page info 
+    # Get article list index 
     pageInfo = soup.find_all(class_='btn')
-    print(pageInfo[3]['href'])
+    first_page_url = pageInfo[3]['href']
+    buf_str = first_page_url.split('index', 1);
+    buf_str = buf_str[1].split('.', 1)
+    max_index = int(buf_str[0])
+#    print(max_index)
+
+    # Get article url for each page
+    for article_list_index in range (max_index - 10, max_index):
+        print(article_list_index)
+        resp = get_board_context(board, article_list_index)
+        soupForEachContext = BeautifulSoup(resp.text, 'html.parser')
+
+        # Get artical from list
+        divs = soupForEachContext.find_all('div', 'r-ent')
+        index = 0
+        for boardContext in divs:
+            index += 1
+            article_link = get_article_link(boardContext)
+            if not article_link is None:
+                get_article_meta_data(article_link)
 
 
-    # Get artical from list
-    divs = soup.find_all('div', 'r-ent')
-    index = 0
-    for link in divs:
-        index += 1
-        result = get_article_link(link)
-        print(result)
-
-    # Artical URL Example: https://www.ptt.cc/bbs/Baseball/M.1509090486.A.1EB.html
-    resp = requests.get(
-        url = 'https://www.ptt.cc/bbs/Baseball/M.1509090486.A.1EB.html',
-        cookies={'over18': '1'}, verify=True, timeout=10
-    )
-    #print(resp.text)
